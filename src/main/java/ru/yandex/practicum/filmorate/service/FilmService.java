d908629eb9181ac909a1f6652f03297ca7455325
DashaKomzolova <darya.komzolova11@yandex.ru>
2026-07-09 23:08:04 +0300

    feat: add workflow with database H2

--- src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FilmService.java
@@ -16,7 +16,6 @@ import ru.yandex.practicum.filmorate.storage.UserStorage;
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Service
